0e6da5b6c0238ffdf77590fae153d1dce6360459
Abhishek Mehra <abhishekmehra@Abhisheks-MacBook-Air.local>
2024-01-07 00:52:38 +0530

    Connected to mongoose to query on MongoDB Atlas

--- app.js
+++ app.js
@@ -1,55 +1,46 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("Connected with MONGODB");
+});
 
+const Hotel = require("./models/hotel.js");
 const app = express();
 
-const hotels = [
-    {
-        id: 1,
-        name: "Orion",
-        city: "Kolkata",
-        area: "Park Street",
-        image: "http://unsplash.com/hotel1"
-    },
-    {
-        id: 2,
-        name: "Taj",
-        city: "Delhi",
-        area: "Saket",
-        image: "http://unsplash.com/hotel2"
-    },
-    {
-        id: 3,
-        name: "Vivanta",
-        city: "Mumbai",
-        area: "BKC",
-        image: "http://unsplash.com/hotel3"
-    }
-]
 app.use(express.json());
+
+
 
 app.get("/", (req,res) => {
     res.send("Hello World!");
 });
 
-app.get("/api/hotels", (req, res) => {
+app.get("/api/hotels", async(req, res) => {
+    const hotels = await Hotel.find();
     res.send(hotels);
 });
 
-app.get("/api/hotels/:id", (req, res) => {
+app.get("/api/hotels/:id", async(req, res) => {
     const id = req.params.id;
-    const hotel = hotels.find((hotel) => hotel.id === parseInt(id));
+    const hotel = await Hotel.findById(id);
     if(!hotel) {
         return res.status(404).send("The hotel with given id was not found!");
     }
     res.send(hotel);
 });
 
-app.post("/api/hotels", (req,res) => {
-    const newHotel = req.body;
-    newHotel.id = hotels.length + 1;
-    hotels.push(newHotel);
-    res.send(newHotel);
+app.post("/api/hotels", async(req,res) => {
+    const hotel = req.body;
+    const dbHotel = await Hotel.create(hotel);
+    res.send(dbHotel);
 
+});
+
+app.delete("/api/hotels/:id", async(req, res) => {
+    const id = req.params.id;
+    const deleteHotel = await Hotel.findByIdAndDelete(id);
+    res.send(deleteHotel);
 })
 app.listen(8080, () => {
     console.log('Server is up and running on port 8080 ....')
---
+++ models/hotel.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const hotelSchema = new mongoose.Schema({
+    name: String,
+    city: String,
+    price: Number,
+    rooms: Number,
+    image: String
+});
+const Hotel = mongoose.model('Hotel', hotelSchema);
+module.exports = Hotel;
